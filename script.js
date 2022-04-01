2fbe93c019cc6a4fd544a4bf5270565ac40c73ec
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-01 00:01:32 -0700

    length and lowercase criteria

--- script.js
+++ script.js
@@ -3,8 +3,24 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var password = "password";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+  var password = "";
+
   // TODO: add code to generate the password here
+  let length = prompt("Enter password length");
+  if (length < 8 || length > 128) {
+    alert("Password should be from 8 to 128 characters long.");
+    console.log(length);
+  }
+
+  let includeLower = alert("Use lowercase letters?");
+  if (confirm(includeLower)) {
+    for (let i = 0; i < length; i++) {
+      var letter = Math.floor(Math.random() * lowercase.length);
+      password += lowercase.substring(letter, letter + 1);
+    }
+  }
 
   return password;
 }
@@ -19,3 +35,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(generatePassword());
