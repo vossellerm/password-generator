efb68195be51f86966a9840eaf06fb6045e4f100
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-01 00:07:40 -0700

    uppercase IF

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
-
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var password = "";
 
   // TODO: add code to generate the password here
   let length = prompt("Enter password length");
   if (length < 8 || length > 128) {
     alert("Password should be from 8 to 128 characters long.");
-    console.log(length);
   }
 
-  let includeLower = alert("Use lowercase letters?");
+  let includeLower = "Use lowercase letters?";
   if (confirm(includeLower)) {
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < 1; i++) {
       var letter = Math.floor(Math.random() * lowercase.length);
       password += lowercase.substring(letter, letter + 1);
+    }
+  }
+
+  let includeUpper = "Use uppercase letters?";
+  if (confirm(includeUpper)) {
+    for (let i = 0; i < 1; i++) {
+      var letter = Math.floor(Math.random() * uppercase.length);
+      password += uppercase.substring(letter, letter + 1);
     }
   }
 
